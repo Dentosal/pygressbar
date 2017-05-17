231956e4315c446883b566ba2e80c2d0eab1a0c6
Hannes Karppila <hannes.karppila@gmail.com>
2017-05-17 15:42:21 +0300

    Add setup.py for pip installation

---
+++ pygressbar/__init__.py
@@ -0,0 +1,1 @@
+from .pygressbar import *
---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name="pygressbar",
+    version="0.1.0",
+    author="Hannes Karppila",
+    license="MIT",
+    url="https://github.com/Dentosal/pygressbar",
+    py_modules=["pygressbar"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Terminals",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
